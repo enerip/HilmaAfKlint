19502a89e1e157e207c43c6d2893fffe0fbc575c
antosp-jpg <antonio.sanchez@alumnos.udit.es>
2024-12-25 16:02:24 +0100

    Avances Works
    Estructura HTML. Falta hacer funcionar carrusel y el responsive.

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// CARRUSEL IMAGENES
+
+$('.responsive').slick({
+  arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
