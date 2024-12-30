2b4c3048d2b540fde2908c904c769a592317fc32
enerip <irene2.pando@gmail.com>
2024-12-30 15:40:18 +0100

    tooltip fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+// TOOLTIP
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 $(document).ready(function () {
   AOS.init();
 });
